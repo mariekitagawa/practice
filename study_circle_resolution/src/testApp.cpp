16d37ae24ed469baf74d02a91699d0e78f2ad5e3
MarieKitagawa <marie.spacenet@gmail.com>
2014-03-09 13:44:37 +0900

    circle_resolution_test

---
+++ study_circle_resolution/src/testApp.cpp
@@ -0,0 +1,74 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    ofSetCircleResolution(5);
+    ofCircle(100, 100, 100);
+
+    ofSetCircleResolution(10);
+    ofCircle(100, 300, 100);
+
+    ofSetCircleResolution(100);
+    ofCircle(100, 500, 100);
+    
+    ofSetCircleResolution(200);
+    ofCircle(400, 500, 100);
+    
+
+    
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
